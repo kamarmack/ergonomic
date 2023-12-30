73f25a858b346bed7490d6185264c81aeaed88c0
Kamar Mack <kamarmack.home@gmail.com>
2023-12-30 10:29:30 +0000

    Make response type more destructure friendly

--- typescript-api-helpers/auth-helpers.ts
+++ typescript-api-helpers/auth-helpers.ts
@@ -3,6 +3,10 @@ import { GeneralizedResponse } from 'ergonomic/typescript-helpers/index.js';
 export type FirebaseAuthCustomTokenParams = {
 	id_token: string;
 };
-export type FirebaseAuthCustomTokenResponse = GeneralizedResponse<{
+export type FirebaseAuthCustomTokenResponseData = {
 	custom_token: string;
-}>;
+};
+export type FirebaseAuthCustomTokenResponse =
+	GeneralizedResponse<FirebaseAuthCustomTokenResponseData> & {
+		readonly data: [FirebaseAuthCustomTokenResponseData];
+	};
