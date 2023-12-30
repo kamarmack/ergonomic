e7c7d93d584fc93657e2eb5713d8a820ba156fb8
Kamar Mack <kamarmack.home@gmail.com>
2023-12-30 09:12:28 +0000

    Refactor logic to log the json response in handleKyError

--- typescript-api-helpers/handleKyError.ts
+++ typescript-api-helpers/handleKyError.ts
@@ -5,17 +5,24 @@ import {
 	getGeneralizedError,
 } from 'ergonomic/typescript-helpers/index.js';
 
-export const handleKyError = async (
-	error: unknown,
-): Promise<GeneralizedResponse> => {
+const logKyError = (error: unknown): void => {
 	console.error(
 		'Encountered an error from ky request:',
 		inspect(error, { depth: 10, colors: true }),
 	);
+};
 
+export const handleKyError = async (
+	error: unknown,
+): Promise<GeneralizedResponse> => {
 	try {
-		return (await (error as HTTPError).response.json()) as GeneralizedResponse;
+		const errorResponse = (await (
+			error as HTTPError
+		).response.json()) as GeneralizedResponse;
+		logKyError(errorResponse);
+		return errorResponse;
 	} catch (err) {
+		logKyError(err);
 		const defaultResponse = {
 			data: [],
 			errors: [getGeneralizedError()],
