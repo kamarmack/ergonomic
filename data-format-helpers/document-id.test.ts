2bbf681dc2da60953e739d8ce073e30760b5d96a
Kamar <kamarmack.home@gmail.com>
2024-04-23 19:28:00 +0000

    Break document IDs

--- data-format-helpers/document-id.test.ts
+++
@@ -1,8 +0,0 @@
-import { shortenObjectName } from 'ergonomic/data-format-helpers/document-id.js';
-
-describe('shortenObjectName', () => {
-	it('should shorten collection names', () => {
-		expect(shortenObjectName('user')).toBe('usr');
-		expect(shortenObjectName('user_api_key')).toBe('usr_api_ky');
-	});
-});
--- data-format-helpers/document-id.ts
+++ data-format-helpers/document-id.ts
@@ -1,87 +1,42 @@
-import * as R from 'ramda';
-import * as UUID from 'uuid';
-import { removeConsecutiveDuplicates } from 'ergonomic/typescript-helpers/string-helpers.js';
+import { v4 } from 'uuid';
+import { compressUUID } from 'ergonomic/data-format-helpers/compressUUID.js';
 
-/**
- * Deterministically shortens an object name.
- *
- * Returns a value that can be prepended to uuids to create document ids e.g. `usr_a1b2c3d4-5e6f-7g8h-9i0j-k1l2m3n4o5p6`
- *
- * @param _object object name
- * @returns {string} shortened name
- *
- * @example
- * shortenObjectName('user') // 'usr'
- * shortenObjectName('asset_account') // 'ast_acnt'
- * shortenObjectName('balance_sheet') // 'blnce_sht'
- * shortenObjectName('credit_line') // 'crdt_lne'
- * shortenObjectName('debt_financing') // 'dbt_fncng'
- */
-export const shortenObjectName = (_object: string): string => {
-	const vowels = new Set(['a', 'e', 'i', 'o', 'u']);
-	const parts = _object.split('_');
-	let shortenedName = '';
-
-	for (const part of parts) {
-		let shortenedPart = '';
-
-		for (let i = 0; i < part.length; i++) {
-			const char = part[i];
-			const prevChar = i > 0 ? part[i - 1] : '';
-
-			if (char && vowels.has(char) && i > 0 && i < part.length - 1) {
-				continue;
-			}
-
-			if (
-				i > 0 &&
-				char === prevChar &&
-				char &&
-				!vowels.has(char) &&
-				i < part.length - 1 &&
-				part[i + 1] === char
-			) {
-				continue;
-			}
-
-			shortenedPart += char;
-		}
-
-		shortenedName +=
-			shortenedPart + (parts.indexOf(part) < parts.length - 1 ? '_' : '');
-	}
-
-	return removeConsecutiveDuplicates(shortenedName);
-};
-
-export const getDocumentIdString = (_object: string): string =>
-	shortenObjectName(_object) + '_' + UUID.v4();
-export const getUUIDFromDocumentId = (_id: string): string => {
-	const splitByUnderscore = _id.split('_');
-	return R.last(splitByUnderscore) as string;
-};
+export const getDocumentIdString = (options: {
+	document_id_prefix: string;
+}): string => options.document_id_prefix + '_' + compressUUID(v4());
 
 const validateDocumentIdString = (
-	_object: string,
 	testValue: string,
+	options: {
+		document_id_prefix: string;
+	},
 ): boolean => {
-	const prefix = shortenObjectName(_object);
-	const suffix = testValue.slice(prefix.length + 1);
-	return UUID.validate(suffix);
+	const split = testValue.split('_');
+	if (split.length !== 2) return false;
+	const [prefix, suffix] = split;
+	if (prefix !== options.document_id_prefix) return false;
+	if (typeof suffix !== 'string') return false;
+	const chars =
+		'0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+	return suffix.split('').every((char) => chars.includes(char));
 };
 
 export const isDocumentIdString = (
-	allowObjects: string[],
+	allowObjects: {
+		document_id_prefix: string;
+	}[],
 	testValue: unknown,
 ): boolean => {
 	if (typeof testValue !== 'string') return false;
 	return allowObjects.some((object) =>
-		validateDocumentIdString(object, testValue),
+		validateDocumentIdString(testValue, object),
 	);
 };
 
 export const isDocumentIdStringRef = (
-	allowObjects: string[],
+	allowObjects: {
+		document_id_prefix: string;
+	}[],
 	testValue: unknown,
 ): boolean => {
 	return testValue === '' || isDocumentIdString(allowObjects, testValue);
