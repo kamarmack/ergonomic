9634748d7aaa4709a7e60545db0c56103f2e8700
Kamar <kamarmack.home@gmail.com>
2024-04-23 19:38:00 +0000

    Patch getIdObj using new document ID logic

--- typescript-api-helpers/getIdObj.ts
+++ typescript-api-helpers/getIdObj.ts
@@ -2,7 +2,14 @@ import * as R from 'ramda';
 import { getDocumentIdString } from 'ergonomic/data-format-helpers/document-id.js';
 
 export const getIdObj = <T extends string>(
-	_object: string,
+	_object: T,
 	obj: Record<T, string>,
+	documentIdPrefixMap: Record<T, string>,
 ): Record<T, string> =>
-	R.mapObjIndexed((_) => getDocumentIdString(_object), obj);
+	R.mapObjIndexed(
+		(_) =>
+			getDocumentIdString({
+				document_id_prefix: documentIdPrefixMap[_object],
+			}),
+		obj,
+	);
